ebb36323f851927e08fa76c0e1e6fc17394d6a0e
parsabee <35123665+parsabee@users.noreply.github.com>
2022-01-26 12:04:49 -0800

    foot print reduction now sets an attribute to the generated parallel op

--- mlir/lib/Dialect/Linalg/Transforms/MemoryFootprintReduction.cpp
+++ mlir/lib/Dialect/Linalg/Transforms/MemoryFootprintReduction.cpp
@@ -24,6 +24,43 @@
 using namespace mlir;
 using namespace mlir::linalg;
 
+constexpr const char *reductionAttrName = "max-memory-footprint";
+
+/// Uses linang tiling rewrite pattern to tile the linalg op,
+/// then adds an attribute specifying it's maximum memory footprint to the
+/// generated loops
+template <typename OpTy>
+struct MemReductionLinalgTilingPattern : public LinalgBaseTilingPattern {
+  const int64_t maxMemFootprint;
+
+  MemReductionLinalgTilingPattern(
+      int64_t maxFootprint, MLIRContext *context, LinalgTilingOptions options,
+      LinalgTransformationFilter filter = LinalgTransformationFilter(),
+      PatternBenefit benefit = 1)
+      : LinalgBaseTilingPattern(OpTy::getOperationName(), context, options,
+                                filter, benefit),
+        maxMemFootprint(maxFootprint) {}
+
+  LogicalResult matchAndRewrite(Operation *op,
+                                PatternRewriter &rewriter) const override {
+    TiledLinalgOp tiledLinalgOp;
+    if (failed(LinalgBaseTilingPattern::matchAndRewriteBase(op, rewriter,
+                                                            tiledLinalgOp)))
+      return failure();
+
+    for (auto *loop : tiledLinalgOp.loops)
+      loop->setAttr(reductionAttrName,
+                    rewriter.getI64IntegerAttr(maxMemFootprint));
+
+    if (tiledLinalgOp.tensorResults.empty())
+      rewriter.eraseOp(op);
+    else
+      rewriter.replaceOp(op, tiledLinalgOp.tensorResults);
+
+    return success();
+  }
+};
+
 // Copied from mlir/lib/Dialect/Linalg/Tiling.cpp
 /// Helper classes for type list expansion.
 template <typename... OpTypes>
@@ -32,37 +69,39 @@ class RewritePatternList;
 template <>
 class RewritePatternList<> {
 public:
-  static void insert(RewritePatternSet &patterns,
+  static void insert(int64_t maxMemoryFootprint, RewritePatternSet &patterns,
                      const LinalgTilingOptions &options) {}
 };
 
 template <typename OpTy, typename... OpTypes>
 class RewritePatternList<OpTy, OpTypes...> {
 public:
-  static void insert(RewritePatternSet &patterns,
+  static void insert(int64_t maxMemoryFootprint, RewritePatternSet &patterns,
                      const LinalgTilingOptions &options) {
     auto *ctx = patterns.getContext();
-    patterns.add<LinalgTilingPattern<OpTy>>(
-        ctx, options,
+    patterns.add<MemReductionLinalgTilingPattern<OpTy>>(
+        maxMemoryFootprint, ctx, options,
         LinalgTransformationFilter(ArrayRef<Identifier>{},
                                    Identifier::get("tiled", ctx)));
-    RewritePatternList<OpTypes...>::insert(patterns, options);
+    RewritePatternList<OpTypes...>::insert(maxMemoryFootprint, patterns,
+                                           options);
   }
 };
 
 // Copied from mlir/lib/Dialect/Linalg/Tiling.cpp
 // Got rid of PadTensorOpTilingPattern
 /// Populate the given list with patterns that apply Linalg tiling.
-static void insertTilingPatterns(RewritePatternSet &patterns,
+static void insertTilingPatterns(int64_t maxMemoryFootprint,
+                                 RewritePatternSet &patterns,
                                  const LinalgTilingOptions &options) {
   RewritePatternList<GenericOp,
 #define GET_OP_LIST
 #include "mlir/Dialect/Linalg/IR/LinalgStructuredOps.cpp.inc"
-                     >::insert(patterns, options);
+                     >::insert(maxMemoryFootprint, patterns, options);
 }
 
 static void
-applyTilingToLoopPatterns(FuncOp funcOp,
+applyTilingToLoopPatterns(int64_t maxMemoryFootprint, FuncOp funcOp,
                           TileSizeComputationFunction tileCompFunc,
                           ArrayRef<StringRef> distributionTypes = {}) {
   auto options = LinalgTilingOptions()
@@ -71,7 +110,7 @@ applyTilingToLoopPatterns(FuncOp funcOp,
                      .setDistributionTypes(distributionTypes);
   MLIRContext *ctx = funcOp.getContext();
   RewritePatternSet patterns(ctx);
-  insertTilingPatterns(patterns, options);
+  insertTilingPatterns(maxMemoryFootprint, patterns, options);
   scf::populateSCFForLoopCanonicalizationPatterns(patterns);
   (void)applyPatternsAndFoldGreedily(funcOp, std::move(patterns));
   (void)applyPatternsAndFoldGreedily(
@@ -107,15 +146,15 @@ struct LinalgMemoryFootprintReductionPass
       // Get the appropriate tiling shape for this generic op, map them to mlir
       // value
       return llvm::to_vector<4>(llvm::map_range(
-          computeTileSizesForMemoryFootprintReduction(theOp,
-                                                      maxMemFootprint,
+          computeTileSizesForMemoryFootprintReduction(theOp, maxMemFootprint,
                                                       reduceFn),
           [&](int64_t s) -> Value {
             return builder.create<ConstantIndexOp>(op->getLoc(), s);
           }));
     };
 
-    applyTilingToLoopPatterns(this->getFunction(), tileCompFunc);
+    applyTilingToLoopPatterns(maxMemFootprint, this->getFunction(),
+                              tileCompFunc);
   }
 };
 } // namespace
@@ -123,6 +162,5 @@ struct LinalgMemoryFootprintReductionPass
 std::unique_ptr<OperationPass<FuncOp>>
 mlir::createLinalgMemoryFootprintReductionPass(int64_t maxFootprint,
                                                MemReduceFn fn) {
-  return std::make_unique<LinalgMemoryFootprintReductionPass>(maxFootprint,
-                                                                 fn);
+  return std::make_unique<LinalgMemoryFootprintReductionPass>(maxFootprint, fn);
 }
