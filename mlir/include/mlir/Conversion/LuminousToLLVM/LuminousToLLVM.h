a0b368ea72de23cb7f9787168fe4d60db34e4a88
Parsa Bagheri <parsabagheri2013@gmail.com>
2022-07-18 12:38:20 -0700

    applied review notes

--- mlir/include/mlir/Conversion/LuminousToLLVM/LuminousToLLVM.h
+++ mlir/include/mlir/Conversion/LuminousToLLVM/LuminousToLLVM.h
@@ -1,6 +1,10 @@
+//===- LuminousToLLVM.h -----------------------------------------*- C++ -*-===//
 //
-// Created by parsab on 7/14/22.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LUMINOUSTOLLVM_H
 #define LLVM_LUMINOUSTOLLVM_H
--- mlir/include/mlir/Conversion/LuminousToStandard/LuminousToStandard.h
+++ mlir/include/mlir/Conversion/LuminousToStandard/LuminousToStandard.h
@@ -1,6 +1,11 @@
+//===- LuminousToStandard.h -------------------------------------*- C++ -*-===//
 //
-// Created by parsab on 6/26/22.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+
 
 #ifndef MLIR_CONVERSION_LUMINOUSTOLLVM_LUMINOUSTOLLVM_H
 #define MLIR_CONVERSION_LUMINOUSTOLLVM_LUMINOUSTOLLVM_H
