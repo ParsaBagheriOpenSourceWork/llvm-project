76f7c94587fc2d1a6a9d3ccb68c2ff11cccbab03
Parsa Bagheri <parsabagheri2013@gmail.com>
2022-07-22 16:31:17 -0700

    fixed lmns-mlir-opt.cpp header

--- mlir/tools/lmns-mlir-opt/lmns-mlir-opt.cpp
+++ mlir/tools/lmns-mlir-opt/lmns-mlir-opt.cpp
@@ -1,4 +1,4 @@
-//===- mlir-opt.cpp - MLIR Optimizer Driver -------------------------------===//
+//===- lmns-mlir-opt.cpp - Luminous host-device code decoupler ------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
